0ae57250499cfd467202b7a36454a0921d2c87ae
neel mani <neelmani.nm@gmail.com>
2018-05-30 15:51:03 +0530

    Webhook for coindesk api

---
+++ webhook.py
@@ -0,0 +1,84 @@
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+# A decorator that tells Flask what URL should trigger our function
+# By default, the flask route responds to the GET requests.
+# This prefrence can be altered by providing methods argument to route() decorator.
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	# Parse the incoming JSON request data and returns it.
+	# By default this function will return None if the mumetype is not application/json but this can be overridden by the force parameter.
+	# force - if set to True the mimetype is ignored
+	# silent - if set to True this methid will fall silently and return None
+	
+	req = request.get_json(silent=True, force=True)
+	print(Json.dumps(req, indent=4)
+	
+	# Extract paprameter value --> query the Open Weather API --> construct response --> send to Dialogflow
+	res = makeResponse(req)
+	res = json.dumps(res, indent = 4)
+	# Setup the response from webhook in right format
+	r = make_response(res) 
+	r.headers['Content-type'] = 'application/json'
+	return r
+	
+# Helper function makeResponse
+
+def makeResponse(req):
+	result = req.get('result')
+	parameters = result.get('parameters')
+	bitcoin = parameters.get('bitcoin')
+	date = parameters.get('date-time')
+	
+	# Condition for extrating the date-time attributes
+	if date.get('startDate'):
+		start_date = date.get('startDate')[:10]
+		end_date = date.get('endDate')[:10]
+	else:
+		start_date = date[:10]
+		end_date = date[:10]
+		query_flag = 'single'
+	
+	base_url = 'https://api.coindesk.com/v1/bpi/historical/close.json?'
+	load_url = 'start=' + start_date + '&end=' + end_date
+	url = base_url + load_url
+	r = request.get(url)
+	json_object = r.json()
+	output_json = json_object['bpi']
+	
+	out = []
+	for i,j in output_json.items():
+		out.append(str(i) + " : "+str('$') +str(j)) 
+	
+	
+	
+	
+			
+	# The response from the service should have the following fields:
+	# Name : speech ; displayText ; source
+	# Type : string ; string ; string
+	# Description : Response to the request
+	# Text displayed on the user device screen ;  Data Source
+	
+	speech = "The Bitcoin price  " + '\n'.join(out)
+	
+	return{
+	"speech":speech"displayText":speech,
+	"source":"apiai-weather-webhook"
+	}
+	
+if __name__ == '__main__':
+	port = int(os.getenv('PORT', 5000))
+	print("Starting app on port %d" % port)
+	app.run(debug=Flase, port=port, host='0.0.0.0')
+	
+	
